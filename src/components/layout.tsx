32db26ae6feb0ccd4e19e8e21a03b532b866e541
henry <henry@bersey.com>
2021-03-08 10:43:18 +0000

    Added Header

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,7 +3,18 @@ import React, { FC } from "react"
 const Layout: FC = ({ children }) => {
   return (
     <div className="flex flex-col">
-      <header></header>
+      <header className="bg-red-500 text-white px-4 pb-1 shadow-lg flex flex-row">
+        <div>
+          <button className="text-3xl">Henry Bersey</button>
+          <div className="text-lg">
+            <button>Programmer</button>
+            &nbsp;|&nbsp;
+            <button>Musician</button>
+          </div>
+        </div>
+          <div className="flex-grow" />
+        <button>GET IN TOUCH</button>
+      </header>
 
       <main className="flex-grow">{children}</main>
 
