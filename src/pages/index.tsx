9b9771527eda0de465443a8b974b8f31520890e3
henry <henry@bersey.com>
2021-03-08 12:19:47 +0000

    Updated Home to make it more responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,16 +5,16 @@ import Layout from "../components/Layout"
 const IndexPage: FC<PageProps> = () => {
   return (
     <Layout>
-      <div className="flex h-full">
-        <div className="m-auto">
-          <h1 className="text-8xl">Henry Bersey</h1>
-          <div className="text-lg text-center flex flex-row pt-16">
-            <div className="flex-grow">
+      <div className="flex h-full text-center">
+        <div className="m-auto w-2/3">
+          <h1 className="lg:text-8xl sm:text-7xl text-5xl">Henry Bersey</h1>
+          <div className="text-lg pt-16 sm:flex block">
+            <div className="sm:w-1/2 sm:mb-0 w-full mb-1">
               <button className="border border-gray-300 rounded px-2 py-1">
                 Programmer
               </button>
             </div>
-            <div className="flex-grow">
+            <div className="sm:w-1/2 w-f">
               <button className="border border-gray-300 rounded px-2 py-1">
                 Musician
               </button>
