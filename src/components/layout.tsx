70ac7af965479eedc37d45b76d0c509af7958a27
henry <henry@bersey.com>
2021-03-09 22:23:45 +0000

    Added links to layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,19 +1,24 @@
 import React, { FC } from "react"
+import { Link } from "gatsby"
 
 const Layout: FC = ({ children }) => {
   return (
     <div className="flex flex-col h-screen divide-y">
       <header className="bg-red-500 text-white px-4 pb-1 shadow-lg flex flex-row">
         <div>
-          <button className="text-3xl">Henry Bersey</button>
+          <Link className="text-3xl" to="/">
+            Henry Bersey
+          </Link>
           <div className="text-lg">
-            <button>Programmer</button>
+            <Link to="/dev">Programmer</Link>
             &nbsp;|&nbsp;
-            <button>Musician</button>
+            <Link to="/music">Musician</Link>
           </div>
         </div>
         <div className="flex-grow" />
-        <button>GET IN TOUCH</button>
+        <Link to="/contact" className="my-auto">
+          GET IN TOUCH
+        </Link>
       </header>
 
       <main className="flex-grow overflow-auto">{children}</main>
